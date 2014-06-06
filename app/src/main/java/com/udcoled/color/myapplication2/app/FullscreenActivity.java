8ab2861298167de319fe961eb32fc1fbc61c4d91
moconnor <moconnor@universaldisplay.com>
2014-06-06 18:42:00 -0400

    Works now with websockets.

--- app/src/main/java/com/udcoled/color/myapplication2/app/FullscreenActivity.java
+++ app/src/main/java/com/udcoled/color/myapplication2/app/FullscreenActivity.java
@@ -2,37 +2,26 @@ package com.udcoled.color.myapplication2.app;
 
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 import android.widget.ImageView;
 
-import android.util.Log;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 
-import com.koushikdutta.async.AsyncServer;
-import com.koushikdutta.async.future.SimpleFuture;
-import com.koushikdutta.async.http.AsyncHttpClient;
-import com.koushikdutta.async.http.AsyncHttpResponse;
-import com.koushikdutta.async.http.socketio.Acknowledge;
-import com.koushikdutta.async.http.socketio.ConnectCallback;
-import com.koushikdutta.async.http.socketio.DisconnectCallback;
-import com.koushikdutta.async.http.socketio.EventCallback;
-import com.koushikdutta.async.http.socketio.JSONCallback;
-import com.koushikdutta.async.http.socketio.ReconnectCallback;
-import com.koushikdutta.async.http.socketio.SocketIOClient;
-import com.koushikdutta.async.http.socketio.SocketIORequest;
-import com.koushikdutta.async.http.socketio.StringCallback;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import junit.framework.TestCase;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+
 public class FullscreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,62 +48,71 @@ public class FullscreenActivity extends Activity {
         //myViewFlipper.setAutoStart(true);
         //myViewFlipper.setFlipInterval(5000);
         //myViewFlipper.startFlipping();
-        SocketIOClient.connect(AsyncHttpClient.getDefaultInstance(), "http://192.168.10.1:70", new ConnectCallback() {
-            @Override
-            public void onConnectCompleted(Exception ex, SocketIOClient client) {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    return;
-                }
-                client.setStringCallback(new StringCallback() {
-                    @Override
-                    public void onString(String string, Acknowledge acknowledge) {
-                        System.out.println(string);
-                    }
-                });
-                client.on("changeSlide", new EventCallback() {
-                    @Override
-                    public void onEvent(JSONArray argument, Acknowledge acknowledge) {
-                        System.out.println("args: " + argument.toString());
-                        myViewFlipper.setDisplayedChild(argument.optInt(0));
-                    }
-                });
-                client.setJSONCallback(new JSONCallback() {
-                    @Override
-                    public void onJSON(JSONObject json, Acknowledge acknowledge) {
-                        System.out.println("json: " + json.toString());
-                    }
-                });
-            }
-        });
+        connectWebSocket();
+        //myViewFlipper.setDisplayedChild(argument.optInt(0));
+
+
     }
     private ViewFlipper myViewFlipper;
     private float initialXPoint;
     int[] image = { R.drawable.fhd_r, R.drawable.fhd_g,
             R.drawable.fhd_b, R.drawable.fhd_black, R.drawable.fhd_w};
 
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                initialXPoint = event.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                float finalx = event.getX();
-                if (initialXPoint > finalx) {
-                    if (myViewFlipper.getDisplayedChild() == image.length)
-                        break;
-                    myViewFlipper.showNext();
-                } else {
-                    if (myViewFlipper.getDisplayedChild() == 0)
-                        break;
-                    myViewFlipper.showPrevious();
-                }
-                break;
+    private WebSocketClient mWebSocketClient;
+    public void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://192.168.10.1:8080");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
         }
-        return false;
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+                //TODO: hide 'reconnect' button
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                Log.i("WebSocket", "Received: " + message);
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        myViewFlipper.setDisplayedChild(Integer.parseInt(message));
+                    }});
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+                //TODO: display 'reconnect' button
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        myViewFlipper.setDisplayedChild(0);
+                    }});
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        myViewFlipper.setDisplayedChild(0);
+                    }});
+                mWebSocketClient.close();
+            }
+        };
+        mWebSocketClient.connect();
     }
+    public void onButtonclick(View v){
+        Button button=(Button) v;
+        connectWebSocket();
+    };
+
 
 }
 
